5a657ee94e17fa0a70dcae34500c274ee4e7c855
Riki <rick.libby.91@gmail.com>
2017-07-24 22:41:24 +0000

    when page loads, wait 3 seconds and add message

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,34 +3,17 @@ import Message from './Message.jsx'
 
 class MessageList extends Component {
 
-  constructor(props) {
-    super(props)
-    
-    let sysMessages = [
-      {
-        'message': 'Anonymous1 changed their name to nomnom.'
-      }
-    ]
-    this.state = {
-      sysMessages: sysMessages
-    }
-  }
-
   render() {
     return (
       <main className="messages">
         {this.props.messages.map(message => {
           return ( 
-            <Message username={message.username} content={message.content} /> 
+            <Message key={message.id} username={message.username} content={message.content} /> 
           )
-        })}
-        {this.state.sysMessages.map(sysMess => {
-          return <div className ="message system">{sysMess.message}</div>
         })}
       </main>
     )
-
   }
-
 }
+
 export default MessageList
