cadcb45f312cc4fb427d45e174506858d13b1573
Riki <rick.libby.91@gmail.com>
2017-07-24 19:47:02 +0000

    add chatbar, message, and messagelist to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav class="navbar">
+          <a href="/" class="navbar-brand">Chatty</a>
+        </nav>
+        <h1>Hello React :)</h1>
+        <ChatBar></ChatBar>
+      </div>
     );
   }
 }
